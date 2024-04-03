9b6c3daf447e47df464b4674b794e7be166b0a1f
Rishabh Singh <pingrishabh@gmail.com>
2024-04-03 20:26:59 +0530

    Setup boilerplate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,32 @@
-/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ["var(--font-inter)", ...fontFamily.sans],
+      },
+      colors: {
+        bg: "#FAFAFA",
+        "gradient-pink": "#F06AEB",
+        "gradient-yellow": "#FF9933",
+        "gray-50": "#F6F6F6",
+        "gray-100": "#EDEDED",
+        "gray-200": "#DADADA",
+        "gray-300": "#C8C8C8",
+        "gray-400": "#B5B5B5",
+        "gray-500": "#A3A3A3",
+        "gray-600": "#828282",
+        "gray-700": "#626262",
+        "gray-800": "#414141",
+        "gray-850": "#31313",
+        "gray-900": "#212121",
+        "gray-950": "#101010",
+        "gray-975": "#080808",
       },
     },
   },
