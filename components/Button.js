c71a599f2f7905e2db3b67ce5321a308c01c04da
Deveshb15 <deveshb583@gmail.com>
2024-04-04 15:38:20 +0530

    added components for landing page

---
+++ components/Button.js
@@ -0,0 +1,42 @@
+import ActivityIndicator from "./ActivityIndicator";
+
+export default function Button({
+  children,
+  className,
+  disabled,
+  icon,
+  loading,
+  onClick,
+  textClassName,
+}) {
+  return (
+    <div
+      className={`flex items-center justify-center rounded-md
+      ${disabled || loading ? "bg-black/50" : "bg-black"} 
+      ${disabled || loading ? "bg-black/50" : "hover:bg-black/80"}
+      ${disabled || loading ? "cursor-default" : "cursor-pointer"}
+      ${!className?.includes("h-") ? "h-[44px]" : null}
+      ${className}
+      `}
+      onClick={() => {
+        if (!disabled && !loading) {
+          onClick();
+        }
+      }}
+    >
+      {loading ? (
+        <ActivityIndicator size="small" />
+      ) : (
+        <>
+          {icon ? icon : null}
+
+          <p
+            className={`text-sm font-medium leading-tight text-white flex items-center justify-center ${textClassName}`}
+          >
+            {children}
+          </p>
+        </>
+      )}
+    </div>
+  );
+}
