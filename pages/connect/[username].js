26342803e4978f332b41ea9f77b68a060d25a56c
Deveshb15 <deveshb583@gmail.com>
2024-05-04 20:54:51 +0530

    check valid urk function fix

--- pages/connect/[username].js
+++ pages/connect/[username].js
@@ -24,7 +24,7 @@ export default function Connect() {
 
     const checkValidUrl = (url) => {
         let urlRegEx = new RegExp(
-            /^(https?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i
+            /^((https?:\/\/)?(([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i
         );
         return urlRegEx.test(url);
     }
