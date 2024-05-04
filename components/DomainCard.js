a7fd4a53777ced426e69d5da3c508de630ab829e
Deveshb15 <deveshb583@gmail.com>
2024-05-04 22:53:31 +0530

    fix: connect flow

--- components/DomainCard.js
+++ components/DomainCard.js
@@ -49,7 +49,7 @@ export default function DomainCard({
                     placeholder={placeholderText ? placeholderText : "https://linklee.xyz/longurl"}
                     label="Destination"
                     onTextChange={(text) => setUrl(text)}
-                    bottomText="You can change this 5 times for free."
+                    bottomText="You can change this 3 times for free."
                     disabled={numberUpdated >= 5}
                 />
                 <Button onClick={buttonAction} className="w-full mt-4">
