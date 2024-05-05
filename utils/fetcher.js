17f4a98e247cea7d9a029d9f4974c74a95e53331
Deveshb15 <deveshb583@gmail.com>
2024-05-05 19:29:36 +0530

    fix: added axios for fetch request

--- utils/fetcher.js
+++ utils/fetcher.js
@@ -1,9 +1,16 @@
-export default async function fetcher(url, payload) {
-  const req = await fetch(url, {
-    body: JSON.stringify(payload),
-    headers: { "Content-Type": "application/json" },
-    method: "POST",
-  });
+import axios from "axios";
 
-  return await req.json();
+// export default async function fetcher(url, payload) {
+//   const req = await fetch(url, {
+//     body: JSON.stringify(payload),
+//     headers: { "Content-Type": "application/json" },
+//     method: "POST",
+//   });
+
+//   return await req.json();
+// }
+
+export default async function fetcher(url, payload) {
+  const req = await axios.post(url, payload);
+  return req.data;
 }
