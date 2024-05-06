b91a9f489a5bba1ebf71e5769e7c6e85127363a2
Deveshb15 <deveshb583@gmail.com>
2024-05-06 10:08:04 +0530

    changed font weight

--- components/Footer.js
+++ components/Footer.js
@@ -9,13 +9,13 @@ export default function Footer() {
             <Row></Row>
             <div className="flex w-full flex-row items-center justify-center bg-bg px-4 py-3">
                 <div
-                    className="text-base font-medium text-black opacity-80"
+                    className="text-base font-normal text-black opacity-80"
                 >
                     built by <a
                         href="https://pratsingh.notion.site/good-people-837f958075cf48b78e45bb311bf0b815"
                         target="_blank"
                         rel="noreferrer"
-                        className="font-extrabold hover:underline">
+                        className="font-bold hover:underline">
                         good people
                     </a>.
                 </div>
