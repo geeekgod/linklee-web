9e0dfd79b6362ec364625c48343dce015162c4e8
Deveshb15 <deveshb583@gmail.com>
2024-05-06 11:22:14 +0530

    added ga

--- components/Page.js
+++ components/Page.js
@@ -1,29 +1,45 @@
 import Head from "next/head";
+import Script from "next/script";
 
 export default function Page({ title }) {
   return (
-    <Head>
-      <title>{title}</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta property="og:title" content="linklee.xyz" />
-      <meta name="twitter:title" content="linklee.xyz" />
-      <meta name="twitter:card" content="summary_large_image" />
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="linklee.xyz" />
+        <meta name="twitter:title" content="linklee.xyz" />
+        <meta name="twitter:card" content="summary_large_image" />
 
-      <meta
-        property="og:description"
-        content={`a link you can redirect anywhere`}
-      />
+        <meta
+          property="og:description"
+          content={`a link you can redirect anywhere`}
+        />
 
-      <meta
-        name="twitter:description"
-        content={`a link you can redirect anywhere`}
-      />
+        <meta
+          name="twitter:description"
+          content={`a link you can redirect anywhere`}
+        />
 
-      <meta name="twitter:image" content={`https://linklee.xyz/og-image.png`} />
+        <meta name="twitter:image" content={`https://linklee.xyz/og-image.png`} />
 
-      <meta property="og:image" content={`https://linklee.xyz/og-image.png`} />
+        <meta property="og:image" content={`https://linklee.xyz/og-image.png`} />
 
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+        <link rel="icon" href="/favicon.ico" />
+
+      </Head>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-7KZD26H6V5"
+      ></Script>
+      <Script id="ga">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-7KZD26H6V5');
+        `}
+      </Script>
+    </>
   );
 }
